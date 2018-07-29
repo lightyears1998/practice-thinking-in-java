3e506df1df99a07b9d2f531868cd4f877b4761fa
lightyears <lightyears1998@hotmail.com>
2018-07-29 10:50:07 +0800

    Hello, world!

---
+++ src/ch2/ex1/Ex1.java
@@ -0,0 +1,12 @@
+package ch2.ex1;
+
+// 练习1
+public class Ex1 {
+	
+	public static void main(String[] args) {
+		Ex1Class ex = new Ex1Class();
+		System.out.println(ex.i);
+		System.out.println(ex.ch);
+	}
+
+}
---
+++ src/ch2/ex1/Ex1Class.java
@@ -0,0 +1,6 @@
+package ch2.ex1;
+
+public class Ex1Class {
+	int i;
+	char ch;
+}
